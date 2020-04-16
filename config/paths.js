aa00d7840b48ac482cc9816c8a355363bcfd3bcd
liuxingyu521 <xuj_power@qq.com>
2020-04-16 12:08:57 +0800

    feat: 增加webpack清除复制插件 & favicon

--- config/paths.js
+++ config/paths.js
@@ -1,11 +1,12 @@
 const path = require('path')
 
-const rootPath = process.cwd();
-const addRootPath = relativePath => path.resolve(rootPath, relativePath)
+const rootPath = process.cwd()
+const addRootPath = (relativePath) => path.resolve(rootPath, relativePath)
 
 module.exports = {
   appPath: rootPath,
   appSrc: addRootPath('src'),
   appViews: addRootPath('views'),
   appDist: addRootPath('dist'),
+  public: addRootPath('public'),
 }
--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,13 +1,15 @@
-const path = require("path");
-const projectPaths = require("./paths");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const projectPaths = require('./paths')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: path.join(projectPaths.appSrc, "index.tsx"),
+  mode: 'development',
+  entry: path.join(projectPaths.appSrc, 'index.tsx'),
   output: {
     path: projectPaths.appDist,
-    filename: "bundle.js",
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -16,28 +18,31 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
           },
         ],
       },
       {
         test: /\.(s[ac]|c)ss$/,
         exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ]
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join(projectPaths.appViews, "index.html"),
+      template: path.join(projectPaths.appViews, 'index.html'),
+      favicon: path.join(projectPaths.public, 'favicon.ico'),
     }),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(projectPaths.public, 'favicon.ico'),
+        to: path.join(projectPaths.appDist, 'favicon.ico'),
+      },
+    ]),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', ".ts", ".tsx", "*"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '*'],
   },
-};
+}
