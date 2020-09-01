40eab35971b2ca3b9b3177eead01c33cfda36929
liuxingyu521 <xuj_power@qq.com>
2020-09-01 09:50:03 +0800

    :bug: 非git 仓储构建信息不加commit id

--- config/util.js
+++ config/util.js
@@ -3,8 +3,16 @@ const { execSync } = require('child_process')
 /** 获取最近一次 commit Hash */
 function getLastCommitHash() {
   const cmdLastCommitHash = 'git rev-parse HEAD'
-  const stdBuffer = execSync(cmdLastCommitHash)
-  return stdBuffer.toString()
+  let hashStr = ''
+
+  try {
+    hashStr = execSync(cmdLastCommitHash).toString()
+  } catch (error) {
+    hashStr = Date.now()
+    console.log('parse git info failed.')
+  }
+
+  return hashStr
 }
 
 module.exports = {
