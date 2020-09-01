f6c901d5dcf5e0b7418709be1cc33d8c3650d44d
liuxingyu521 <xuj_power@qq.com>
2020-09-01 10:08:37 +0800

    :recycle: 去掉 test 代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import App from './app/index'
-import { Print } from './test/decorator'
-import './test/hooks'
-
-const print = new Print()
-print.exec()
-
-console.log(process.env.NODE_ENV)
 
 render(<App />, document.getElementById('root'))
